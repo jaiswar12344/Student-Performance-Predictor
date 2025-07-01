b0182a9f0ace6ea43738e2dad6a1efad48278a0c
Rohan jaiswar <jaiswarrohan184@gmail.com>
2025-07-01 16:25:24 +0530

    Fix: Final working version with model and preprocessor

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,7 +1,9 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+import pickle
 
 
 class PredictPipeline:
@@ -11,7 +13,7 @@ class PredictPipeline:
     def predict(self,features):
         try:
             model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            preprocessor_path=os.path.join('artifacts','proprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
